f586e19045a46a51e51d56a996a07c66175161fe
Hiroto-Iizuka <oroxith@gmail.com>
2025-03-30 20:35:05 +0900

    Adding our first functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 module Bookshelf
   class Routes < Hanami::Routes
-    # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
+    root to: "home.index"
   end
 end
